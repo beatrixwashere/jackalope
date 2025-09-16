443d094fa1e0c4ed55b21817da31fecf4dc0dbd2
beatrixwashere <127708031+beatrixwashere@users.noreply.github.com>
2025-09-16 04:14:19 -0500

    fix replays and make input files cleaner

--- jackalope.cs
+++ jackalope.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Reflection;
 using System.Text;
 using BepInEx;
 using BepInEx.Logging;
@@ -22,19 +23,21 @@ public class jackalope : BaseUnityPlugin
 
     public static bool tasReplay = false;
 
+    public static bool tasResetting = false;
+
     public static Text statsDisplay;
 
     public static StringBuilder timerBuild = new StringBuilder(256);
 
-    public static GameObject mainCharacter;
+    public static GameObject mchar;
 
-    public static Character mainCharacterScript;
+    public static Character mcharScript;
 
-    public static Rigidbody2D mainCharacterBody;
+    public static Rigidbody2D mcharBody;
+
+    public static MethodInfo mcharIR;
 
     public static List<float[]> inputs; // [up, down, left, right, jump, sprint, suicide, dance]
-
-    public static Dictionary<int, List<float[]>> savestates;
 
     private void Awake()
     {
@@ -47,13 +50,8 @@ public class jackalope : BaseUnityPlugin
         Harmony.CreateAndPatchAll(typeof(jackalope));
         Logger.LogInfo($"applied patches!");
 
-        inputs = new List<float[]>();
-        savestates = new Dictionary<int, List<float[]>>();
-        for (int i = 0; i < 10; i++)
-        {
-            savestates.Add(i, []);
-        }
-        Logger.LogInfo($"set up savestates!");
+        inputs = [];
+        Logger.LogInfo($"set up environment!");
     }
 
     [HarmonyPatch(typeof(Character), "Awake")]
@@ -68,9 +66,10 @@ public class jackalope : BaseUnityPlugin
             if (obj.name == "NotAMeatboy(Clone)")
             {
                 Debug.Log("character connected!");
-                mainCharacter = GameObject.Find("NotAMeatboy(Clone)");
-                mainCharacterScript = mainCharacter.GetComponent<Character>();
-                mainCharacterBody = mainCharacter.GetComponent<Rigidbody2D>();
+                mchar = GameObject.Find("NotAMeatboy(Clone)");
+                mcharScript = mchar.GetComponent<Character>();
+                mcharBody = mchar.GetComponent<Rigidbody2D>();
+                mcharIR = typeof(Character).GetMethod("ReceiveEvent");
             }
         }
     }
@@ -105,14 +104,6 @@ public class jackalope : BaseUnityPlugin
             Debug.Log("gamespeed: " + Time.timeScale);
         }
 
-        // reset time
-        if (Input.GetKeyDown(KeyCode.Slash))
-        {
-            Debug.Log("reset");
-            tasFrames = 0;
-            inputs = new List<float[]>();
-        }
-
         // replay
         if (Input.GetKeyDown(KeyCode.Minus))
         {
@@ -123,50 +114,12 @@ public class jackalope : BaseUnityPlugin
         // fast forward
         if (Input.GetKeyDown(KeyCode.Equals))
         {
-            Time.timeScale = Time.timeScale != 5.0f ? 5.0f : 1.0f;
+            Time.timeScale = Time.timeScale < 5.0f ? 5.0f : 1.0f;
             Debug.Log("fast forward: " + (Time.timeScale == 5.0f));
         }
 
-        // savestates
-        for (int i = 48; i < 58; i++)
-        {
-            if (Input.GetKeyDown((KeyCode)i))
-            {
-                if (Input.GetKey(KeyCode.LeftShift))
-                {
-                    savestates[i - 48] = [.. inputs];
-                    Debug.Log("saved state " + (i - 48) + "! length = " + savestates[i - 48].Count);
-                }
-                else
-                {
-                    inputs = [.. savestates[i - 48]];
-                    tasFrames = 0;
-                    Debug.Log("loaded state " + (i - 48) + "! length = " + inputs.Count);
-                }
-            }
-        }
-
-        // export
-        if (Input.GetKeyDown(KeyCode.LeftBracket))
-        {
-            using (StreamWriter sw = File.CreateText(@"C:\Program Files (x86)\Steam\steamapps\common\Ultimate Chicken Horse\tas.txt"))
-            {
-                for (int i = 0; i < inputs.Count; i++)
-                {
-                    string nextline = "";
-                    for (int j = 0; j < 8; j++)
-                    {
-                        nextline += inputs[i][j] + ",";
-                    }
-                    sw.WriteLine(nextline);
-                    Debug.Log(nextline);
-                }
-            }
-            Debug.Log("exported!");
-        }
-
         // import
-        if (Input.GetKeyDown(KeyCode.RightBracket))
+        if (Input.GetKeyDown(KeyCode.Slash))
         {
             tasFrames = 0;
             inputs = new List<float[]>();
@@ -175,19 +128,41 @@ public class jackalope : BaseUnityPlugin
                 using (StreamReader sr = File.OpenText(@"C:\Program Files (x86)\Steam\steamapps\common\Ultimate Chicken Horse\tas.txt"))
                 {
                     string nextline = "";
+                    string[] keychecks = { "w", "s", "a", "d", "j", "k", "l", "m" };
+                    inputs.Add(new float[8]);
                     while ((nextline = sr.ReadLine()) != null)
                     {
-                        inputs.Add(new float[8]);
-                        string[] vals = nextline.Split(",", System.StringSplitOptions.RemoveEmptyEntries);
-                        for (int i = 0; i < vals.Length; i++)
+                        string[] vals = nextline.Split(":", System.StringSplitOptions.RemoveEmptyEntries);
+                        float[] currentinput = new float[8];
+                        for (int i = 0; i < keychecks.Length; i++)
                         {
-                            inputs[tasFrames][i] = (float)Convert.ToDouble(vals[i]);
+                            currentinput[i] = vals[1].Contains(keychecks[i]) ? 1 : 0;
                         }
-                        tasFrames++;
+                        for (int hold = 0; hold < Convert.ToInt32(vals[0]); hold++)
+                        {
+                            inputs.Add(currentinput);
+                            tasFrames++;
+                        }
                     }
                 }
                 tasFrames = 0;
                 Debug.Log("imported!");
+
+                tasReplay = true;
+                //tasPause = false;
+                //Time.timeScale = 1.0f;
+
+                InputEvent[] e =
+                [
+                    new InputEvent(0, InputEvent.InputKey.Left, 1, true),
+                    new InputEvent(0, InputEvent.InputKey.Left, 0, true),
+                    new InputEvent(0, InputEvent.InputKey.Right, 1, true),
+                    new InputEvent(0, InputEvent.InputKey.Right, 0, true),
+                ];
+                for (int i = 0; i < e.Length; i++)
+                {
+                    mcharIR.Invoke(mcharScript, [e[i]]);
+                }
             }
             else
             {
@@ -196,31 +171,60 @@ public class jackalope : BaseUnityPlugin
         }
     }
 
-    [HarmonyPatch(typeof(Character), "fullUpdate")]
+    [HarmonyPatch(typeof(Character), "FixedUpdate")]
     [HarmonyPrefix]
     static void TASReplay()
     {
         if (tasReplay)
         {
             // read inputs
-            if (inputs.Count > tasFrames && mainCharacter != null)
+            if (inputs.Count > tasFrames && mchar != null)
             {
                 Debug.Log("replaying...");
-                mainCharacterScript.up = inputs[tasFrames][0];
-                mainCharacterScript.down = inputs[tasFrames][1];
-                mainCharacterScript.left = inputs[tasFrames][2];
-                mainCharacterScript.right = inputs[tasFrames][3];
-                mainCharacterScript.jump = inputs[tasFrames][4] == 1;
-                mainCharacterScript.sprint = inputs[tasFrames][5] == 1;
-                mainCharacterScript.suicide = inputs[tasFrames][6] == 1;
-                mainCharacterScript.dance = inputs[tasFrames][7] == 1;
+                InputEvent[] e =
+                [
+                    new InputEvent(0, InputEvent.InputKey.Up, inputs[tasFrames][0], true),
+                    new InputEvent(0, InputEvent.InputKey.Down, inputs[tasFrames][1], true),
+                    new InputEvent(0, InputEvent.InputKey.Left, inputs[tasFrames][2], true),
+                    new InputEvent(0, InputEvent.InputKey.Right, inputs[tasFrames][3], true),
+                    new InputEvent(0, InputEvent.InputKey.Jump, inputs[tasFrames][4] == 1, mcharScript.jump != (inputs[tasFrames][4] == 1)),
+                    new InputEvent(0, InputEvent.InputKey.Sprint, inputs[tasFrames][5] == 1, mcharScript.sprint != (inputs[tasFrames][5] == 1)),
+                    new InputEvent(0, InputEvent.InputKey.Suicide, inputs[tasFrames][6] == 1, mcharScript.suicide != (inputs[tasFrames][6] == 1)),
+                    new InputEvent(0, InputEvent.InputKey.Inventory, inputs[tasFrames][7] == 1, mcharScript.dance != (inputs[tasFrames][7] == 1)),
+                ];
+                for (int i = 0; i < e.Length; i++)
+                {
+                    mcharIR.Invoke(mcharScript, [e[i]]);
+                }
             }
             else
             {
                 tasPause = true;
                 Time.timeScale = 0.0f;
+                tasReplay = false;
             }
         }
+        if (tasResetting)
+        {
+            // disable reset mode
+            if (Time.timeScale == 1.0f)
+            {
+                tasResetting = false;
+                tasPause = true;
+                tasFrames = 0;
+                inputs = new List<float[]>();
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(Character), "Disable")]
+    [HarmonyPrefix]
+    static void TASReset()
+    {
+        Debug.Log("resetting...");
+        Time.timeScale = 2.0f;
+        tasResetting = true;
+        tasPause = false;
     }
 
     [HarmonyPatch(typeof(DigitalClock), "ShowSecondsAsTime")]
@@ -245,7 +249,7 @@ public class jackalope : BaseUnityPlugin
         }
 
         // update stats display
-        else if (mainCharacter != null)
+        else if (mchar != null)
         {
             timerBuild.Length = 0;
             int num = Mathf.FloorToInt(tasFrames / 60f / 60f);
@@ -257,8 +261,8 @@ public class jackalope : BaseUnityPlugin
 
             statsDisplay.text = "// jackalope //\n";
             statsDisplay.text += "time: " + timerBuild.ToString() + " (" + tasFrames + "f)\n";
-            statsDisplay.text += "position: (" + mainCharacter.transform.position.x + ", " + mainCharacter.transform.position.y + ")\n";
-            statsDisplay.text += "velocity: (" + mainCharacterBody.velocity.x + ", " + mainCharacterBody.velocity.y + ")\n";
+            statsDisplay.text += "position: (" + mchar.transform.position.x + ", " + mchar.transform.position.y + ")\n";
+            statsDisplay.text += "velocity: (" + mcharBody.velocity.x + ", " + mcharBody.velocity.y + ")\n";
             statsDisplay.text += "gamespeed: " + Time.timeScale + "x\n";
         }
     }
@@ -267,17 +271,6 @@ public class jackalope : BaseUnityPlugin
     [HarmonyPostfix]
     static void TASUpdate()
     {
-        // write inputs
-        inputs.Add(new float[8]);
-        inputs[tasFrames][0] = mainCharacterScript.up;
-        inputs[tasFrames][1] = mainCharacterScript.down;
-        inputs[tasFrames][2] = mainCharacterScript.left;
-        inputs[tasFrames][3] = mainCharacterScript.right;
-        inputs[tasFrames][4] = mainCharacterScript.jump ? 1 : 0;
-        inputs[tasFrames][5] = mainCharacterScript.sprint ? 1 : 0;
-        inputs[tasFrames][6] = mainCharacterScript.suicide ? 1 : 0;
-        inputs[tasFrames][7] = mainCharacterScript.dance ? 1 : 0;
-
         // update frame count
         tasFrames += 1;
     }
